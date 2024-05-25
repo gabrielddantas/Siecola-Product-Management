2ae7676d9bd2b209d966cf4b6fd0316c081dac5a
gabrielddantas <gabrielddanttas@gmail.com>
2024-05-25 19:20:27 -0300

    config(Docker): Initial commit and automate docker image

---
+++ productManagementService/src/main/java/com/gabrielddantas/productManagementService/ProductManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.gabrielddantas.productManagementService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductManagementServiceApplication.class, args);
+	}
+
+}
